c168c19a85c295052d0a73eeb8cced32a787b190
xxcosmos <xandcosmos@gmail.com>
2019-05-26 11:56:35 +0800

    改进首页样式

--- pages/course/course.js
+++ pages/course/course.js
@@ -100,6 +100,7 @@ Page({
             commentList: response.commentVOList,
             averageRate: response.averageRate
         })
+        console.log(response.commentVOList)
         Toast.clear()
     },
 
